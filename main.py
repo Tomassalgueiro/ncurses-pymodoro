4ff8cd25c8a632e2871da77e2c7366183b761193
guinhas <tomasthor8@gmail.com>
2026-02-16 02:06:20 +0000

    initial commit - starting the pomodoro logic

---
+++ main.py
@@ -0,0 +1,31 @@
+import curses
+import time
+from curses import wrapper
+
+#stdscr = curses.initscr()
+
+#def main(stdscr):
+
+#   curses.noecho()
+#   curses.cbreak()
+#   stdscr.keypad(True)
+#   curses.nocbreak()
+#   stdscr.keypad(False)
+#   curses.echo()
+#   curses.endwin()
+
+#    stdscr.clear()
+
+#    for i in range(0,11):
+#        v = i-10
+#        stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10/v))
+
+#        stdscr.refresh()
+#        stdscr.getkey()
+
+# wrapper(main)
+
+# POMODORO LOGIC FIRST
+#
+#
+#
