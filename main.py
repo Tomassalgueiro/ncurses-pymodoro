f14398e5d1b3dd274d25a78d05d316d2c78cb6fb
guinhas <tomasthor8@gmail.com>
2026-02-16 11:37:25 +0000

    refactored the pomodor class to a different file and started the curses implementation

--- main.py
+++ main.py
@@ -1,83 +1,44 @@
 import curses
 import time
+import pomodoro
 from curses import wrapper
 
-#stdscr = curses.initscr()
 
-#def main(stdscr):
+def format_time(seconds):
+    minute = seconds // 60
+    sec = seconds % 60
+    return f"{minute:02}:{sec:02}"
 
-#   curses.noecho()
-#   curses.cbreak()
-#   stdscr.keypad(True)
-#   curses.nocbreak()
-#   stdscr.keypad(False)
-#   curses.echo()
-#   curses.endwin()
+def main(stdscr):
+    curses.curs_set(0)
+    stdscr.timeout(100)
+    
+    # create pomodoro object (check pomodoro.py for more info on parameters)
+    p1 = pomodoro.Pomodoro(3,1,5,3,12)
 
-#    stdscr.clear()
+    last_tick_time = time.time()
 
-#    for i in range(0,11):
-#        v = i-10
-#        stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10/v))
+    while True:
+        
+        key = stdscr.getch()
+        if key == ord('q'):
+            break
+        elif key == ord(' '):
+            p1.toggle()
 
-#        stdscr.refresh()
-#        stdscr.getkey()
+        current_time = time.time()
+        if current_time - last_tick_time >= 1.0:
+            p1.tick()
+            last_tick_time = current_time
+        
+        stdscr.erase()
 
-# wrapper(main)
+        stdscr.addstr(0,0, f"Mode: {p1.mode}")
+        stdscr.addstr(1,0, f"Remaining: {format_time(p1.remaining)}")
+        stdscr.addstr(3,0, f"Space = Start/Pause")
+        stdscr.addstr(4,0, f"Q = Quit")
+        
+        stdscr.refresh()
 
-# POMODORO LOGIC FIRST
-class Pomodoro:
-    # this implementation follows the method:
-    # work small break 3 times and then work and a big break (usually 20 min, but may add option to change it later)
 
-    def __init__(self, small_break=3, big_break=1, time_session=1500, time_small_break=300, time_big_break=1200):
-        self.small_break = small_break
-        self.big_break = big_break
-        self.time_session = time_session
-        self.time_small_break = time_small_break
-        self.time_big_break = time_big_break
-        self.count = 0
-        self.mode = "work"
-        self.running = False
-        self.remaining = time_session
-
-    def toggle(self):
-        self.running = not self.running
-
-    def tick(self):
-        if not self.running:
-            return
-
-        self.remaining -=1
-        if self.remaining <= 0:
-            self.switch_mode()
-
-    def switch_mode(self):
-        if self.mode == "work":
-            if self.count >= self.small_break:
-                self.mode = "big_break"
-                self.remaining = self.time_big_break 
-                self.count = 0
-            else:
-                self.mode = "small_break"
-                self.remaining = self.time_small_break 
-                self.count += 1
-        else:
-            self.mode = "work"
-            self.remaining =  self.time_session
-
-p1 = Pomodoro(3,1,5,3,12)
-
-p1.toggle()
-flag = False
-while 1:
-    print(p1.remaining)
-    p1.tick()
-    time.sleep(1)
-    if flag == True:
-        print("swapping mode")
-        flag = False
-    if p1.remaining == 1:
-        flag = True
-print("finished")
-
+wrapper(main)
