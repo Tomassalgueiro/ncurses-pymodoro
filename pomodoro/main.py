d9d518efb041e046e6b110dd1570a9660501c46b
guinhas <tomasthor8@gmail.com>
2026-05-12 21:31:52 +0100

    Used some ai to help on the final product

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -13,126 +13,134 @@ import number_design
 CLOCK_SIZE_Y = 5
 CLOCK_SIZE_X = 17
 
+
 # identify the audio file
 # to change it swap alarm.wav in the config 
-from importlib import resources
+ASSET_DIR = Path(__file__).parent / "assets"
+ALARM = ASSET_DIR / "alarm.wav"
 
-with resources.path("assets", "alarm.wav") as file_path:
-    data, fs = sf.read(file_path, dtype='float32')
-
-# finds the directory 
-directory = Path.home()/".config"/"ncurses_pymodoro"
-file = directory/"config.txt"
-
-# make sure the file
-directory.mkdir(parents=True, exist_ok=True)
-
-# create file if it doesn't exist
-if not file.exists():
-    with open(file, "w") as config:
-        config.write("3\n")
-        config.write("1\n")
-        config.write("1500\n")
-        config.write("300\n")
-        config.write("1200\n")
-    config_val = [3,1,1500,300,1200]
-else:
-    with open(file, "r") as config:
-        config_val = [int(line.strip()) for line in config.readlines()]        
+def main():
 
 
+    data, fs = sf.read(ALARM, dtype='float32')
 
-def get_text_color(mode):
-    match mode:
-        # this will output red
-        case "Work":
-            return 1
-        case "Small Break":
-            return 2
-        case "Big Break":
-            return 3
+    # finds the directory 
+    directory = Path.home()/".config"/"ncurses_pymodoro"
+    file = directory/"config.txt"
 
-def format_time(seconds):
-    minute = seconds // 60
-    sec = seconds % 60
-    return f"{minute:02}:{sec:02}"
+    # make sure the file
+    directory.mkdir(parents=True, exist_ok=True)
 
-def main(stdscr):
+    # create file if it doesn't exist
+    if not file.exists():
+        with open(file, "w") as config:
+            config.write("3\n")
+            config.write("1\n")
+            config.write("1500\n")
+            config.write("300\n")
+            config.write("1200\n")
+        config_val = [3,1,1500,300,1200]
+    else:
+        with open(file, "r") as config:
+            config_val = [int(line.strip()) for line in config.readlines()]        
 
-    curses.curs_set(0)
-    stdscr.timeout(100)
-    curses.start_color()
-    
-    # define the 3 possible colorschemes
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    
-    # create pomodoro object (check pomodoro.py for more info on parameters)
-    # hardcoded version
-    #p1 = pomodoro.Pomodoro(3,1,1500,300,1200)
-    # insert .config version in here    
-    p1 = pomodoro.Pomodoro(config_val[0],config_val[1],config_val[2],config_val[3],config_val[4])
-    last_tick_time = time.time()
 
-    while True:
+
+    def get_text_color(mode):
+        match mode:
+            # this will output red
+            case "Work":
+                return 1
+            case "Small Break":
+                return 2
+            case "Big Break":
+                return 3
+
+    def format_time(seconds):
+        minute = seconds // 60
+        sec = seconds % 60
+        return f"{minute:02}:{sec:02}"
+
+    def main(stdscr):
+
+        curses.curs_set(0)
+        stdscr.timeout(100)
+        curses.start_color()
         
-        screen_size_x = curses.COLS
-        screen_size_y = curses.LINES
+        # define the 3 possible colorschemes
+        curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
         
-        key = stdscr.getch()
-        if key == ord('q'):
-            break
-        elif key == ord(' '):
-            p1.toggle()
-        elif key == ord('t') and not p1.running:
-            p1.switch_mode()
-        # implement helper window if the program is not running
-        # this will call another window and pressing backspace will go back
-        # elif key == ord('h') and not p1.running: 
-        elif key == curses.KEY_RESIZE:
-            curses.update_lines_cols()
-            stdscr.clear()
+        # create pomodoro object (check pomodoro.py for more info on parameters)
+        # hardcoded version
+        #p1 = pomodoro.Pomodoro(3,1,1500,300,1200)
+        # insert .config version in here    
+        p1 = pomodoro.Pomodoro(config_val[0],config_val[1],config_val[2],config_val[3],config_val[4])
+        last_tick_time = time.time()
 
-
-        current_time = time.time()
-        if current_time - last_tick_time >= 1.0:
-            play_alarm = p1.tick()
-            last_tick_time = current_time
-        else:
-            play_alarm = False
-
-        stdscr.erase()
-        time_str = format_time(p1.remaining)
-        
-        # try block to avoid crashes in case the terminal is too small
-        try:
-            stdscr.addstr(screen_size_y // 3 + 6,screen_size_x // 2 - 17 // 2, f"Mode: {p1.mode}") 
-            stdscr.addstr(screen_size_y // 3 + 7,screen_size_x // 2 - 17 // 2, f"Count: {p1.count}") 
-            stdscr.addstr(screen_size_y // 3 + 8,screen_size_x // 2 - 17 // 2, f"Cycles: {p1.cycles}") 
-            stdscr.addstr(screen_size_y // 3 + 9,screen_size_x // 2 - 17 // 2, f"Space = Start/Pause")
-            stdscr.addstr(screen_size_y // 3 + 10,screen_size_x // 2 - 17 // 2, f"Q = Quit")
+        while True:
             
-            current_x = screen_size_x // 2 - CLOCK_SIZE_X // 2  
-            for char in time_str:
-                if char == ":":
-                    stdscr.addstr(screen_size_y // 3 - 1, current_x, "█", curses.color_pair(get_text_color(p1.mode)))
-                    stdscr.addstr(screen_size_y // 3 + 1, current_x, "█", curses.color_pair(get_text_color(p1.mode)))
-                    current_x += 2
-                else:
-                    pattern = number_design.num_dict[int(char)]
-                    for i, bit in enumerate(pattern):
-                        if bit:
-                            stdscr.addstr((screen_size_y // 3 - 2 + (i//3)), current_x + (i % 3), "█", curses.color_pair(get_text_color(p1.mode)))
-                    current_x += 4
+            screen_size_x = curses.COLS
+            screen_size_y = curses.LINES
+            
+            key = stdscr.getch()
+            if key == ord('q'):
+                break
+            elif key == ord(' '):
+                p1.toggle()
+            elif key == ord('t') and not p1.running:
+                p1.switch_mode()
+            # implement helper window if the program is not running
+            # this will call another window and pressing backspace will go back
+            # elif key == ord('h') and not p1.running: 
+            elif key == curses.KEY_RESIZE:
+                curses.update_lines_cols()
+                stdscr.clear()
 
-        # might add a messsage saying "terminal size too small", not important right now
-        except curses.error:
-            stdscr.addstr(5,5, f"Screen Size Too small")
 
-        stdscr.refresh()
-        if play_alarm == True:
-            sd.play(data,fs)
-            sd.wait()
+            current_time = time.time()
+            if current_time - last_tick_time >= 1.0:
+                play_alarm = p1.tick()
+                last_tick_time = current_time
+            else:
+                play_alarm = False
 
-wrapper(main)
+            stdscr.erase()
+            time_str = format_time(p1.remaining)
+            
+            # try block to avoid crashes in case the terminal is too small
+            try:
+                stdscr.addstr(screen_size_y // 3 + 6,screen_size_x // 2 - 17 // 2, f"Mode: {p1.mode}") 
+                stdscr.addstr(screen_size_y // 3 + 7,screen_size_x // 2 - 17 // 2, f"Count: {p1.count}") 
+                stdscr.addstr(screen_size_y // 3 + 8,screen_size_x // 2 - 17 // 2, f"Cycles: {p1.cycles}") 
+                stdscr.addstr(screen_size_y // 3 + 9,screen_size_x // 2 - 17 // 2, f"Space = Start/Pause")
+                stdscr.addstr(screen_size_y // 3 + 10,screen_size_x // 2 - 17 // 2, f"Q = Quit")
+                
+                current_x = screen_size_x // 2 - CLOCK_SIZE_X // 2  
+                for char in time_str:
+                    if char == ":":
+                        stdscr.addstr(screen_size_y // 3 - 1, current_x, "█", curses.color_pair(get_text_color(p1.mode)))
+                        stdscr.addstr(screen_size_y // 3 + 1, current_x, "█", curses.color_pair(get_text_color(p1.mode)))
+                        current_x += 2
+                    else:
+                        pattern = number_design.num_dict[int(char)]
+                        for i, bit in enumerate(pattern):
+                            if bit:
+                                stdscr.addstr((screen_size_y // 3 - 2 + (i//3)), current_x + (i % 3), "█", curses.color_pair(get_text_color(p1.mode)))
+                        current_x += 4
+
+            # might add a messsage saying "terminal size too small", not important right now
+            except curses.error:
+                stdscr.addstr(5,5, f"Screen Size Too small")
+
+            stdscr.refresh()
+            if play_alarm == True:
+                sd.play(data,fs)
+                sd.wait()
+
+    wrapper(main)
+
+
+if __name__ == "__main__":
+    main()
